5ee6a2658314f9de1d7b94e0e57fd8247f2c3638
Tyler Chong <tylrchong@gmail.com>
2017-06-04 00:24:06 -1000

    MongoDB routes semi-integrated

--- routes/app.js
+++ routes/app.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var path = require('path');
 var multer = require('multer');  // tool to parse data that isnt text
+var MongoClient = require('mongodb');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,6 +19,19 @@ router.post('/upload', upload.any(), (req, res) => {
 
   console.log(req.files);
 
+  // MongoClient.connect('mongodb://test:test@ds161551.mlab.com:61551/gamesforgifts', function (err, db) {
+  //   if (err) {
+  //     return console.error('Connection Error. @mongodb');
+  //   }
+  //   try {
+  //     db.collection('games').insert(req.body);
+  //   } catch (err) {
+  //     console.error('Error Inserting. @mongodb');
+  //     return res.send('lol u wat m8');
+  //   }
+  //   db.close();
+  // });
+
   res.json(req.files.map(file => {
     let ext = path.extname(file.originalname);
     return {
@@ -27,6 +41,27 @@ router.post('/upload', upload.any(), (req, res) => {
   }));
 
 });
+
+
+
+router.get('/game/:id', (req, res) => {
+  MongoClient.connect('mongodb://test:test@ds161551.mlab.com:61551/gamesforgifts', function (err, db) {
+			if (err) {
+				return console.error('Connection Error. @mongodb');
+			}
+			db.collection('games').findOne({id: req.params.id}, function (err, obj) {
+        if (err) return console.error('We fucked up big time');
+        if (obj) {
+          console.log(obj.id);
+          db.close();
+          return res.send(obj.id); // Send back the fucking entire object ya clown
+        }
+        db.close();
+        return res.send("Wat"); // No game found
+      });
+		});
+});
+
 
 router.get('/', function (req, res, next) {
     res.render('index');
